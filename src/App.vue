b5418b11c0b63624789a03a43afa6d41892364dd
MayDay <2747254700@qq.com>
2020-10-30 08:33:44 +0800

    更改css文件的引入路径

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
-    
+    <router-view></router-view>
+    <main-tab-bar />
   </div>
 </template>
 
 <script>
-
+import MainTabBar from 'components/content/mainTabbar/MainTabbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MainTabBar
+  }
 }
 </script>
 
 <style>
-  @import './assets/css/base.css';
+  /* 使用 import 引入 css 文件时要在路径前面加一个 ~ */
+  @import '~assets/css/base.css';
 </style>

d1c26c7669e67f1ae69e3da44ee818e9018256b6
MayDay <2747254700@qq.com>
2020-10-30 08:30:09 +0800

    初始话导航栏组件

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- 这个组件只管理有关 TabBar 相关的内容，而 TabBarItem 的内容完全可以不用管理 -->
+    <div id="tab-bar">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+@import '~assets/css/base.css';
+#tab-bar {
+  display: flex;
+  background: #f6f6f6;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, .1);
+}
+
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red'
+    }
+  },
+  data () {
+    return {
+      // isActive: false
+    }
+  },
+  methods: {
+    itemClick () {
+      // console.log('aa')
+      this.$router.push(this.path)
+    }
+  },
+  computed: {
+    isActive () {
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle () {
+      return this.isActive? { color: this.activeColor } : {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  /* 一般导航栏高度都设置为49px */
+  height: 49px;
+  text-align: center;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  /* vertical-align 属性用于去除图片下面的空间 */
+  vertical-align: middle;
+  margin-top: 3px;
+  margin-bottom: 2px;
+}
+</style>

5251f1c8a5b6faf942a41fbca633b67c4f5b1a54
MayDay <2747254700@qq.com>
2020-11-01 10:06:42 +0800

    在Home视图中使用Recommend组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,7 +4,8 @@
     <nav-bar class="header">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
+    <home-swiper :banners="banners" />
+    <home-recommends :recommends="recommends" />
   </div>
 </template>
 
@@ -12,6 +13,7 @@
 import NavBar from 'components/common/navbar/NavBar' 
 import { getHomeMultidata } from 'network/home'
 import HomeSwiper from './childComps/HomeSwiper'
+import HomeRecommends from './childComps/HomeRecommend'
 export default {
   data () {
     return {
@@ -21,12 +23,12 @@ export default {
   },
   components: {
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    HomeRecommends
   },
   created () {
     getHomeMultidata()
       .then(res => {
-        console.log(res.data.data)
         this.banners = res.data.data.banner.list
         this.recommends = res.data.data.recommend.list
       })

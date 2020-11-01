7840d747ed5a735e096b0f73e53b09f9e7f9de1a
MayDay <2747254700@qq.com>
2020-11-01 18:09:54 +0800

    在Home视图中使用GoodsList组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,7 +7,8 @@
     <home-swiper :banners="banners" />
     <home-recommends :recommends="recommends" />
     <home-feather />
-    <tab-control class="tab-control" :control="['流行', '流行', '流行']" />
+    <tab-control class="tab-control" :control="['流行', '新款', '精选']" />
+    <goods-list :goods-list="goods['pop'].list"/>
     <ul>
       <li>2</li>
       <li>1</li>
@@ -66,33 +67,57 @@
 <script>
 import NavBar from 'components/common/navbar/NavBar' 
 import TabControl from 'components/content/tabControl/TabControl'
+import GoodsList from 'components/content/goods/GoodsList'
 
 import HomeSwiper from './childComps/HomeSwiper'
 import HomeRecommends from './childComps/HomeRecommend'
 import HomeFeather from './childComps/HomeFeather'
 
-import { getHomeMultidata } from 'network/home'
+import { getHomeMultidata, getHomeData } from 'network/home'
 export default {
   data () {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goods: {
+        'pop': { page: 0, list: [] },
+        'new': { page: 0, list: [] },
+        'sell': { page: 0, list: [] }
+      }
     }
   },
   components: {
     NavBar,
     TabControl,
+    GoodsList,
 
     HomeSwiper,
     HomeRecommends,
     HomeFeather
   },
   created () {
-    getHomeMultidata()
-      .then(res => {
-        this.banners = res.data.data.banner.list
-        this.recommends = res.data.data.recommend.list
-      })
+    this.ggetHomeMultidata()
+    this.ggetHomeData('pop')
+    this.ggetHomeData('new')
+    this.ggetHomeData('sell')
+  },
+  methods: {
+    ggetHomeMultidata () {
+      getHomeMultidata()
+        .then(res => {
+          this.banners = res.data.data.banner.list
+          this.recommends = res.data.data.recommend.list
+        })
+    },
+    ggetHomeData (type) {
+      console.log(this.goods[type].page + 1)
+      const page = this.goods[type].page + 1
+      getHomeData(type, page)
+        .then(res => {
+          this.goods[type].list.push(1,3)
+          this.goods[type].page++
+        })
+    }
   }
 }
 </script>

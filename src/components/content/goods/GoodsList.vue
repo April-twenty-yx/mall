c0abcb4b44c44d98732970a8190f7ff47f271a40
MayDay <2747254700@qq.com>
2020-11-01 18:07:17 +0800

    新增商品展示组件

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="goods-list">
+    <goods-list-item v-for="item in goodsList.list" :key="item.id" :good-item="item"/>
+  </div>
+</template>
+
+<script>
+import GoodsListItem from './GoodsListItem'
+export default {
+  props: {
+    goodsList: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  components: {
+    GoodsListItem
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
---
+++ src/components/content/goods/GoodsListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="good-item">
+    <img :src="goodItem.img" alt="">
+    <div>
+      <p>{{ goodItem.title }}</p>
+      <span>{{ goodItem.price }}</span>
+      <span>{{ goodItem.collect }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    goodItem: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>

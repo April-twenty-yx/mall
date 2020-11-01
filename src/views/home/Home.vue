65451c9d010fadd83312b55a635479556b061755
MayDay <2747254700@qq.com>
2020-11-01 08:45:56 +0800

    在视图文件Home中使用NavBar组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    Home
+  <!-- Home 视图唯一，此处可以用 id -->
+  <div id="home">
+    <nav-bar class="header">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-  
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style scoped>
-  
+  .header {
+    background: var(--color-tint);
+    color: #fff;
+  }
 </style>

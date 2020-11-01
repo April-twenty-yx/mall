df0dca2791f7b709068489b13dbe8f280fbab932
MayDay <2747254700@qq.com>
2020-11-01 10:06:03 +0800

    添加Recommend组件

---
+++ src/views/home/childComps/HomeRecommend.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-item" v-for="item in recommends" :key="item.link">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{ item.title }}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    recommends: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    padding: 15px 0 25px;
+    border-bottom: 10px solid #eee;
+    font-size: 12px;
+  }
+  .recommend-item {
+    flex: 1;
+  }
+  .recommend-item img {
+    width: 65px;
+    height: 65px;
+    margin-bottom: 10px;
+  }
+</style>
